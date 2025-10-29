69d438d4642d2338462ade019fa38440f6f7d2da
Rene Bucher <133756710+reneb1404@users.noreply.github.com>
2025-10-29 17:00:33 +0000

    Added dependencies and basic boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-	return null;
+	return (
+		<div className="my-6 px-4 max-w-md mx-auto">
+			<div className="text-center space-y-6">
+				<h1 className="text-3xl font-bold">Welcome to my App!</h1>
+				<Button>
+					<Link href="/auth/login"> Sign In / Sign Up</Link>
+				</Button>
+			</div>
+		</div>
+	);
 }
