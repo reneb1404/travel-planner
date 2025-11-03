2ca13c0f4b24f17480433f6f4628c843a9598701
Rene Bucher <133756710+reneb1404@users.noreply.github.com>
2025-11-03 16:46:56 +0000

    Added auth logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+
 export default function Home() {
-	return null;
+	const { data: session, isPending: loading } = authClient.useSession();
+
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div className="my-6 px-4 max-w-md mx-auto">
+			<div className="text-center space-y-6">
+				{session == null ? (
+					<>
+						<h1 className="text-3xl font-bold">Welcome to Our App</h1>
+						<Button asChild size="lg">
+							<Link href="/auth/login">Sign In / Sign Up</Link>
+						</Button>
+					</>
+				) : (
+					<>
+						<h1 className="text-3xl font-bold">Welcome {session.user.name}!</h1>
+						<div className="flex gap-4 justify-center">
+							<Button
+								variant={"destructive"}
+								onClick={async () => await authClient.signOut()}
+							>
+								Logout
+							</Button>
+						</div>
+					</>
+				)}
+			</div>
+		</div>
+	);
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { db } from "@/drizzle/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+	database: drizzleAdapter(db, {
+		provider: "pg",
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	session: {
+		cookieCache: {
+			enabled: true,
+			maxAge: 60,
+		},
+	},
+	plugins: [nextCookies()],
+});
